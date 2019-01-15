6773fd80622e29722042e3008397c236739d033c
Kristofer Dowdell <kristofer_dowdell@me.com>
2019-01-14 19:26:47 -0500

    Initial version

---
+++ php/send_email_form.php
@@ -0,0 +1,17 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "kris@krisdowdell.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first-name'];
+    $last_name = $_POST['last-name'];
+    $subject = $_POST['subject'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From: $from";
+	$headers .= "Reply-To: $from";
+
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . " for getting in touch!";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
