052b9d285649ad47556f6024f8dc415a7e610edb
Kristofer Dowdell <kristofer_dowdell@me.com>
2019-01-16 21:41:41 -0500

    Modified bottom margin for h2 elements. Changed the html background image to be darker.

--- js/prevent_reload.js
+++ js/prevent_reload.js
@@ -1,8 +1,9 @@
+//Prevents page redirection after clicking Submit button
 $('#contactform').on('submit', function(e) {
 
     e.preventDefault();
 
-    var formData = new FormData(this);
+    let formData = new FormData(this);
 
     $.ajax({
         method:'POST',
@@ -11,18 +12,40 @@ $('#contactform').on('submit', function(e) {
         cache:false,
         contentType: false,
         processData: false,
-        success:function(data){
-            alert(data);
-
-        },
+        success: thankYouSubmit,
         error: function(data){
             alert('Sorry, something went wrong!');
-        }
+        },
     });
 });
 
+//Resets form after submission
 const contactForm = document.getElementById('contactform');
 
 contactForm.addEventListener('submit', function () {
 	contactForm.reset();
 });
+
+
+//Thank You message after clicking Submit
+
+//Targets div containing Submit button
+const thankYouSection = document.getElementById('thank-you-submit');
+
+//Targest contact's First Name in form
+const contactFirstName = document.getElementById('first-name').value;
+
+function thankYouSubmit() {
+	//Creates div element with .form-group and .col-md-9 classes
+	let createDiv = document.createElement('div');
+	createDiv.classList.add('form-group');
+	createDiv.classList.add('col-md-9');
+	
+	//Creates p tag with Thank You message
+	let createParTag = document.createElement('p');
+	createParTag.innerHTML = 'Thank you ' + contactFirstName + ' for getting in touch!';
+	
+	//Adds new div and p tags to document
+	thankYouSection.appendChild(createDiv);
+	createDiv.appendChild(createParTag);
+};
